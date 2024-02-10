cabf4ec9ea7f64720d73622f30c9001bf74e4aaa
zora-git <154859133+zora-git@users.noreply.github.com>
2024-02-10 18:56:27 +0900

    react test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="weather-app">
+      <header>
+        <form classNAme="search-form" id="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="search-form-input"
+            id="search-form-input"
+            required
+          />
+          <input type="submit" value="Search" className="search-form-button" />
+        </form>
       </header>
+      <main>
+        <div className="weather-app-data">
+          <div>
+            <h1 id="city">Seoul</h1>
+            <p>
+              <span id="time">Saturday 18:51</span>
+              <span id="weather-description">clear sky</span>
+              <br />
+              Humidity: <strong id="humidity">60</strong>, Wind:
+              <strong id="wind-speed">3.6km/h</strong>
+            </p>
+          </div>
+          <div className="weather-app-temperature-container">
+            <div className="weather-app-emoji" id="weather-emoji">
+              🌥️
+            </div>
+            <div className="weather-app-temperature" id="temperature">
+              3
+            </div>
+            <div className="weather-app-unit">°C</div>
+          </div>
+        </div>
+        <div className="weather-forecast" id="forecast"></div>
+      </main>
+      <footer>
+        Coded by <a href="https://github.com/zora-git">HJ Park</a>, code hosted
+        on
+        <a href="https://github.com/zora-git/weather-forecast">GitHub</a>
+        and website hosted on
+        <a href="https://hj-weather-forecast.netlify.app">Netlify</a>
+      </footer>
     </div>
   );
 }
